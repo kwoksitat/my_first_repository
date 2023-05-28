0111f98600160d17fe546cfc0de28f4f38cbb6f4
kwoksitat <guosida998@126.com>
2023-05-28 17:57:25 +0800

    first commit

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"我叫郭思达"<<endl;
+    cout<<"我叫贾礎宁"<<endl;
+    cout<<"my husband is guosida"<<endl;
+    cout<<"my wife is jiachuning"<<endl;
+
+    return 0;
+}
